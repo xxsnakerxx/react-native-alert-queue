e871bb00309d139a47ee854fdcd7e5206ae0155f
Dmitrii Kolesnikov <xxsnakerxx@gmail.com>
2025-04-26 20:36:01 +0200

    feat: add react-native-safe-area-context dependency and update App component for safe area handling

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,29 +1,90 @@
-import { Text, View, StyleSheet, Pressable } from 'react-native';
+import { Text, View, StyleSheet, Pressable, ScrollView } from 'react-native';
 
 import { alert, AlertContainer } from 'react-native-alert-queue';
 
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Pressable
-        onPress={() =>
-          alert.show({
-            title: 'Hello',
-            message: 'I am an alert',
-          })
-        }
-      >
-        <Text>Show Alert</Text>
-      </Pressable>
-      <AlertContainer />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <SafeAreaView style={styles.wrapper}>
+          <Text style={styles.title}>React Native Alert Queue</Text>
+          <ScrollView style={styles.scrollView}>
+            <View style={styles.section}>
+              <Text style={styles.sectionTitle}>Basics</Text>
+              <Button
+                onPress={() =>
+                  alert.show({
+                    title: 'Hello',
+                    message: 'I am an alert',
+                  })
+                }
+                text="Show Alert"
+              />
+              <Button
+                onPress={() => {
+                  alert.show({
+                    title: 'Alert 1',
+                    message: 'I am an alert 1',
+                  });
+                  alert.show({
+                    title: 'Alert 2',
+                    message: 'I am an alert 2',
+                  });
+                }}
+                text="Show 2 Alerts"
+              />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+        <AlertContainer />
+      </View>
+    </SafeAreaProvider>
   );
 }
+
+const Button = ({ onPress, text }: { onPress: () => void; text: string }) => {
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{text}</Text>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  wrapper: {
+    flex: 1,
+  },
+  scrollView: {
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginVertical: 20,
+    textAlign: 'center',
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+  section: {
+    gap: 10,
+  },
+  button: {
+    backgroundColor: '#f0f0f0',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
